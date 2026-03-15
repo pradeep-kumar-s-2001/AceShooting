1322a7bd65deebd035fb8548790b06df57cddc37
Sroopesh1122 <shivuroopesh6362@gmail.com>
2026-03-15 14:34:49 +0530

    Initial commit from local

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'  // Import the Home component
+
+function App() {
+  return (
+    <>
+      <Home />  {/* This will render your homepage */}
+    </>
+  )
+}
+
+export default App
